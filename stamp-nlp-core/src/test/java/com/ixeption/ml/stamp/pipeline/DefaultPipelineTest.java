1827706f214140b5536b20dec295a0027d1a3586
digital-thinking <github@digital-thinking.de>
2018-02-06 14:31:31 +0100

    add junit

---
+++ stamp-nlp-core/src/test/java/com/ixeption/ml/stamp/pipeline/DefaultPipelineTest.java
@@ -0,0 +1,14 @@
+package com.ixeption.ml.stamp.pipeline;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class DefaultPipelineTest {
+    @Test
+    public void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
